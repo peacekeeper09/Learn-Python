2c65e25064032903c6a1f71bf22615f74f8abaa5
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2022-12-30 19:07:07 +0530

    Create data type.py

---
+++ data type.py
@@ -0,0 +1,15 @@
+#this is an example of str data type.
+x = str("This is an example")
+
+#this is an example of int data type.
+x = int(0709)
+
+#this is an example of float data type.
+x = float(07.09)
+
+#this is an example of complex data type.
+x = complex(2i)
+
+#this is an example of list data type.
+x = [1,2,3,4]
+x = ['a', 'b', 1, 2]
