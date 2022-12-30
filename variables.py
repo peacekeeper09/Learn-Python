827620573dd076daea8eff5a00bcd1947bd171f5
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2022-12-30 08:12:44 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,14 @@
+var_name = data
+    #or
+var_name = "data"
+
+#Declaring a string value in variable.
+testing = "some value in string"
+print(testing)
+#output = some value in string
+
+
+#Declaring an integer value in variable.
+testing = 9
+print(testing)
+#output = 9
