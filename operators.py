04a3bbd1e7b1fd7f3ecd4a529cb920076bb487b7
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2022-12-31 08:33:19 +0530

    Update operators.py

--- operators.py
+++ operators.py
@@ -1,1 +1,30 @@
+#Arithmetic Operators
+#addition
+x = 7
+y = 9
+print(x+y)
+#output => 16
 
+#subtraction
+print(x-y)
+#output => -2
+
+#multiplication
+print(x*y)
+#output => 63
+
+#division
+print(x/y)
+#output => 0.7777
+
+#modulus
+print(x%y)
+#output => 7
+
+#exponentiation	
+print(x**y)
+#output => 40353607
+
+#floor division
+print(x//y)
+#output => 0
