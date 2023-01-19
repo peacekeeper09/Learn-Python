e9a99764da28c4a89c1d21a742dbffff26d65118
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2023-01-19 10:52:47 +0530

    Create input.py

---
+++ input.py
@@ -0,0 +1,8 @@
+#This is how you can ask for input in Python.
+
+a = int(input("This is how you can ask for an integer input from user."))
+
+a = str(input("This is how you can ask for a string input from user."))
+
+a = input("This is how you can ask for any input from user.")
+
