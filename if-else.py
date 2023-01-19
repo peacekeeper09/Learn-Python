fb438af61c275026c2ed3e0cdfdbcf19e883d587
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2023-01-19 11:21:09 +0530

    Create if-else.py

---
+++ if-else.py
@@ -0,0 +1,16 @@
+#This is an example how you can use if-else in Python.
+
+a = 17
+if a > 18:
+  print("You are eligible for voting.")
+else:
+  print("You are not eligible for voting.")
+  
+#Another example 
+
+a = int(input("Enter your age:- "))
+if a > 18:
+  print("You are eligible for voting.")
+else:
+  print("You are not eligible for voting.")
+  
