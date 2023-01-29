568b249266875135b1d66a474083f0b4d46edffc
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2023-01-29 20:44:21 +0530

    Create break statement.py

---
+++ break statement.py
@@ -0,0 +1,11 @@
+# let's make a list.
+a = ['PEACE', '09', '07']
+for x in a:
+    print(x)
+    if x == "09":
+        break
+
+#this will print all the element inside a in the following manner :- 
+
+#PEACE
+#09
