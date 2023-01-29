e3f714e8142a19c0d2956e6b67c2e86a555e222b
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2023-01-29 21:07:24 +0530

    Create continue statement.py

---
+++ continue statement.py
@@ -0,0 +1,11 @@
+# let's make a list.
+a = ['PEACE', '09', '07']
+for x in a:
+  if x == "PEACE":
+    continue
+  print(x)
+
+#this will print all the element inside a in the following manner :- 
+
+#09
+#07
